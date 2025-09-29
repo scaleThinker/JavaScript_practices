416ca30a922f9cafa2e102ba9365033ba1d1d870
Brendo Tavares <brendotavares@gmail.com>
2025-09-29 17:48:17 -0300

    Inicia js mod 2

---
+++ js-curso-2-aula1/app.js
@@ -0,0 +1,7 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número e 1 a 10';
+function verificarChute() {
+    console.log ('O botão foi clicado');
+}
